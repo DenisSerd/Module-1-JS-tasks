7273e900e7ae5e6ce4e27993d49c504ae8c3b995
Denis Serdyuk <serdyuk.itrum345@gmail.com>
2023-02-09 16:11:44 +0300

    fixTask13

--- task13.js
+++ task13.js
@@ -1,5 +1,17 @@
 const array = ["str", 123, "123", true, null, {}, 2];
 const funct = (ar) => {
-  ar.forEach(item => console.log(typeof item));
+  ar.forEach(item => {
+    switch (typeof item){
+      case 'string': console.log('Это строка')
+        break;
+      case 'number': console.log('Это число')
+        break;
+      case 'boolean': console.log('Это булево значение')
+        break;
+      default:
+        console.log('Неизвестное значение');
+        break;
+    };}
+  );
 };
 funct(array);
